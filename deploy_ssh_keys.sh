9409cb52684c51299685229e858887e53e849603
Pang-Duck <131836756+Pang-Duck@users.noreply.github.com>
2024-11-26 15:11:34 +0900

    로컬호스트 호출 삭제

--- deploy_ssh_keys.sh
+++ deploy_ssh_keys.sh
@@ -6,7 +6,7 @@ HOSTS_FILE="./hosts"
 SSH_KEY="$HOME/.ssh/id_rsa.pub"
 
 # hosts 파일에서 ansible_host IP 추출
-IPS=$(grep -E "^\s*[^#]" "$HOSTS_FILE" | awk '{for (i=1; i<=NF; i++) if ($i ~ /ansible_host=/) {split($i, arr, "="); print arr[2]}}')
+IPS=$(grep -E "^\s*[^#]" "$HOSTS_FILE" | grep -v install | awk '{for (i=1; i<=NF; i++) if ($i ~ /ansible_host=/) {split($i, arr, "="); print arr[2]}}')
 
 for IP in $IPS; do
     echo "Copying SSH key to $IP..."
