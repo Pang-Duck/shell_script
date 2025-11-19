1682bb978905a4984087320495dfc29f798b753c
Pang-Duck <131836756+Pang-Duck@users.noreply.github.com>
2025-11-19 17:20:56 +0900

    repo 업데이트 수정

--- repo-autoupdate.sh
+++ repo-autoupdate.sh
@@ -4,8 +4,11 @@
 
 RPM_DIR="/repo/rpm"
 DEB_DIR="/repo/deb"
+EXCLUDE_REGEX='\.repodata|Packages\.gz|Packages'
 
-inotifywait -m -e create -e delete -e moved_to -e moved_from "$RPM_DIR" "$DEB_DIR" |
+inotifywait -m -e create -e delete -e moved_to -e moved_from \
+    --exclude "$EXCLUDE_REGEX" \
+    "$RPM_DIR" "$DEB_DIR" |
 while read -r path action file; do
     echo "[INFO] Change detected: $file ($action) in $path"
 
@@ -13,15 +16,16 @@ while read -r path action file; do
     if [[ "$path" == "$RPM_DIR/"* ]]; then
         echo "[INFO] Updating RPM metadata..."
         createrepo --update "$RPM_DIR"
-        echo "[INFO] RPM metadata updated."     
+        echo "[INFO] RPM metadata updated."
     fi
 
     # DEB repo 변경
     if [[ "$path" == "$DEB_DIR/"* ]]; then
         echo "[INFO] Updating DEB metadata..."
-        cd "$DEB_BASE"
-        dpkg-scanpackages -m $DEB_DIR > Packages
-        dpkg-scanpackages -m $DEB_DIR | gzip -9 > Packages.gz
+        cd "$DEB_DIR"
+        dpkg-scanpackages -m . > Packages
+        dpkg-scanpackages -m . | gzip -9 > Packages.gz
+        cd - > /dev/null
         echo "[INFO] DEB metadata updated."
     fi
 done
